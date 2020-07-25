f57008d353d0057646709d60ca494ce92992b37a
Il Harper <afanyiyu@hotmail.com>
2020-07-25 23:09:53 +0800

    feat: Synchronizer Update

--- Core/Synchronizer.cs
+++ Core/Synchronizer.cs
@@ -1,12 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using Ruminoid.Common.Timing;
 
 namespace Ruminoid.LIVE.Core
 {
-    class Synchronizer
+    public sealed class Synchronizer : INotifyPropertyChanged
     {
+        #region Current
+
+        public static Synchronizer Current { get; } = new Synchronizer();
+
+        #endregion
+
+        #region DataContext
+
+        private bool _loaded;
+
+        public bool Loaded
+        {
+            get => _loaded;
+            set
+            {
+                _loaded = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private Position _position;
+
+        public Position Position
+        {
+            get => _position;
+            set
+            {
+                _position = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region PropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
     }
 }
