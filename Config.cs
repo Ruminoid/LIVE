81682212181fc2240bf191f0e3587683e9467155
Il Harper <afanyiyu@hotmail.com>
2020-07-25 20:39:23 +0800

    feat: Config Class Update

--- Config.cs
+++ Config.cs
@@ -1,12 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Ruminoid.Common.Helpers;
 
 namespace Ruminoid.LIVE
 {
-    class Config
+    [RuminoidProduct("Dashboard")]
+    [JsonObject(MemberSerialization.OptIn)]
+    public sealed class Config : INotifyPropertyChanged
     {
+        #region Current
+
+        public static Config Current { get; set; } = ConfigHelper<Config>.OpenConfig();
+
+        #endregion
+
+        #region PropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
     }
 }
