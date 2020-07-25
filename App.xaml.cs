ec4a40e0de522e5fb51bb01094c923c5714ec174
Il Harper <afanyiyu@hotmail.com>
2020-07-25 11:52:39 +0800

    feat: App.xaml.cs Update

--- App.xaml.cs
+++ App.xaml.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using MetroRadiance.UI;
+using Ruminoid.LIVE.Windows;
 
 namespace Ruminoid.LIVE
 {
@@ -13,5 +16,44 @@ namespace Ruminoid.LIVE
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            DispatcherUnhandledException += (sender, args) =>
+            {
+                args.Handled = true;
+                MessageBox.Show(
+                    args.Exception.Message,
+                    "灾难性故障",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error,
+                    MessageBoxResult.OK);
+            };
+
+            AppDomain.CurrentDomain.UnhandledException += (sender, args) =>
+            {
+                MessageBox.Show(
+                    ((Exception)args.ExceptionObject)?.Message ?? "Exception",
+                    "灾难性故障",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error,
+                    MessageBoxResult.OK);
+            };
+
+            LIVE.Properties.Resources.Culture = CultureInfo.CurrentUICulture;
+
+            if (MainWindow is null) MainWindow = new MainWindow();
+            MainWindow.Show();
+
+            Current.Dispatcher?.Invoke(() => ThemeService.Current.ChangeTheme(Theme.Dark));
+            Current.Dispatcher?.Invoke(() => ThemeService.Current.ChangeAccent(Accent.Blue));
+
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            ThemeService.Current.Register(this, Theme.Windows, Accent.Windows);
+        }
     }
 }
