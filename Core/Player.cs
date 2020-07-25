18bca089b8dd393b11d3d959a6af9bcaa3b7ee4a
Il Harper <afanyiyu@hotmail.com>
2020-07-26 01:59:30 +0800

    feat: Player Update

--- Core/Player.cs
+++ Core/Player.cs
@@ -3,10 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NAudio.Wave;
 
 namespace Ruminoid.LIVE.Core
 {
-    class Player
+    public sealed class Player : IDisposable
     {
+        #region Core Data
+
+        private AudioFileReader _audioFile;
+
+        public WaveOutEvent AudioOutput;
+
+        #endregion
+
+        #region Constructor
+
+        public Player(string audioPath)
+        {
+            // Initialize Core
+            _audioFile = new AudioFileReader(audioPath);
+            AudioOutput = new WaveOutEvent();
+            AudioOutput.Init(_audioFile);
+        }
+
+        #endregion
+
+        public void Dispose()
+        {
+            AudioOutput.Stop();
+            AudioOutput?.Dispose();
+            _audioFile?.Dispose();
+        }
     }
 }
