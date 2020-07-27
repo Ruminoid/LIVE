efa3b239e53c2ad62ee661c9f3576ccf0d4a43e5
Il Harper <afanyiyu@hotmail.com>
2020-07-27 15:00:30 +0800

    feat: MemoryMonitor Update

--- Core/MemoryMonitor.cs
+++ Core/MemoryMonitor.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Ruminoid.Common.Utilities;
 
 namespace Ruminoid.LIVE.Core
 {
@@ -20,16 +21,36 @@ namespace Ruminoid.LIVE.Core
 
         #endregion
 
+        #region State Data
+
+        private int _memSize;
+
+        private WorkingState _warningState;
+
+        private WorkingState WarningState
+        {
+            get => _warningState;
+            set
+            {
+                if (_warningState != value) StateChanged?.Invoke(this, value);
+                _warningState = value;
+            }
+        }
+
+        #endregion
+
         #region Constructors
 
-        public MemoryMonitor(double moniState)
+        public MemoryMonitor(int memSize)
         {
+            // Initialize State
+            _memSize = memSize;
+            WarningState = WorkingState.Completed;
+
+            // Initialize Core
             _currentProcess = Process.GetCurrentProcess();
             _timer = new Timer(
-                (state =>
-                {
-                    if (getMemory() > moniState) Purge?.Invoke(this, EventArgs.Empty);
-                }),
+                TimerCallback,
                 null,
                 2000,
                 Timeout.Infinite);
@@ -37,9 +58,30 @@ namespace Ruminoid.LIVE.Core
 
         #endregion
 
+        #region Methods
+
+        private void TimerCallback(object state)
+        {
+            double mem = getMemory();
+
+            if (mem > _memSize)
+            {
+                Purge?.Invoke(this, EventArgs.Empty);
+                WarningState = WorkingState.Failed;
+            }
+            else if (mem > _memSize - 50)
+                WarningState = WorkingState.Working;
+            else
+                WarningState = WorkingState.Completed;
+        }
+
+        #endregion
+
         #region Event Trigger
 
         public event EventHandler Purge;
+
+        public event EventHandler<WorkingState> StateChanged;
 
         #endregion
 
