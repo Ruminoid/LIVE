f2d24ba7372d6fac7c88b29c28749012ff68c653
Il Harper <afanyiyu@hotmail.com>
2020-07-28 20:51:35 +0800

    feat: Add Key Bindings

--- Windows/MainWindowCommandBindings.cs
+++ Windows/MainWindowCommandBindings.cs
@@ -5,7 +5,9 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
+using Ruminoid.LIVE.Core;
 
 namespace Ruminoid.LIVE.Windows
 {
@@ -15,12 +17,27 @@ namespace Ruminoid.LIVE.Windows
 
         private void MainWindow_OnPreviewKeyDown(object sender, KeyEventArgs e)
         {
-            // Ignore
-        }
-
-        private void MainWindow_OnPreviewKeyUp(object sender, KeyEventArgs e)
-        {
-            // Ignore
+            if ((Keyboard.Modifiers & ModifierKeys.Control) == ModifierKeys.Control) return;
+            if (e.Key == Key.L ||
+                e.Key == Key.Space ||
+                e.Key == Key.Left ||
+                e.Key == Key.Right) e.Handled = true;
+            switch (e.Key)
+            {
+                case Key.L:
+                    if (Synchronizer.Current.Loaded) LoadToggle_OnUnchecked(Wnd1, new RoutedEventArgs());
+                    else LoadToggle_OnChecked(Wnd1, new RoutedEventArgs());
+                    break;
+                case Key.Space:
+                    if (Synchronizer.Current.Loaded) Synchronizer.Current.Playing = !Synchronizer.Current.Playing;
+                    break;
+                case Key.Left:
+                    if (Synchronizer.Current.Loaded) Synchronizer.Current.JumpDuration(-1000);
+                    break;
+                case Key.Right:
+                    if (Synchronizer.Current.Loaded) Synchronizer.Current.JumpDuration(+1000);
+                    break;
+            }
         }
 
         #endregion
